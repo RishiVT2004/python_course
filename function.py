01ae3527d1bad00d31ed9a41b997134b8cdd41c5
RishiVT2004 <121601369+RishiVT2004@users.noreply.github.com>
2024-02-09 00:56:48 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,53 @@
+'''
+ def calc(num1, num2):
+    command = str(input(f'enter operator : '))
+    if command == '+':
+        print(f'result{num1 + num2}')
+    elif command == '-':
+        print(f'result is {num1 - num2}')
+    elif command == '*':
+        print(f'result is {num1 * num2}')
+    elif command == '/':
+        print(f'result is {num1 / num2}')
+    else:
+        return 'invalid'
+
+
+n1 = float(input('enter number 1: '))
+n2 = float(input('enter number 2: '))
+
+calc(n1, n2)
+'''
+import math
+
+'''
+
+try:
+    def bonus_calc(hours):
+        salary = float(input(f'enter salary : '))
+        if hours<100:
+            return salary
+        elif 100<hours<150:
+            print(f'{salary*0.2}')
+        else:
+            print(f'{salary*0.33}')
+except ValueError:
+    print(f'invalid value')
+
+
+bonus_calc(180)
+
+
+'''
+
+
+def fibonacci(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    count = fibonacci(n-1) + fibonacci(n-2)
+    return count
+
+
+print(fibonacci(35))
