f0636cb978d6887d6573db1269246c606a2cc70b
RishiVT2004 <121601369+RishiVT2004@users.noreply.github.com>
2024-02-09 20:34:39 +0530

    Update oops.py

--- oops.py
+++ oops.py
@@ -1,1 +1,12 @@
 '''Inheritence'''
+class Info:
+    def __init__(self, name):
+        self.name = name
+
+    def talk(self):
+        print(f'you are allowed to talk : {self.name}')
+
+
+i1 = Info('rishi')
+print(i1.name)
+i1.talk()
