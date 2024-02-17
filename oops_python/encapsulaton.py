247fe1c6cb02b54b68bcd51a54f3618589cf310f
Rishivatsal Mishra <121601369+RishiVT2004@users.noreply.github.com>
2024-02-17 21:33:29 +0530

    Add files via upload

---
+++ oops_python/encapsulaton.py
@@ -0,0 +1,16 @@
+'''
+Encapsulation in Python is a concept that binds the data (attributes) and the methods (functions) that manipulate the data into a single unit or class.
+It restricts direct access to some of the object's components, which helps prevent unintended interference and misuse.
+
+In Python, encapsulation is achieved by using private variables and methods. Private variables are declared by prefixing an attribute or method with
+double underscores (__). These variables are not accessible directly from outside the class. Instead, they are accessed through getter and setter methods,
+which allows for controlled access and modification of the data.
+
+Encapsulation helps in data hiding, as the internal representation of an object is hidden from the outside world. This allows the object to maintain
+its internal state without being directly manipulated by external code. It also helps in reducing the complexity of the code and makes it easier to
+maintain and debug.
+
+Overall, encapsulation is a fundamental principle of object-oriented programming that promotes better code organization,
+ reusability, and security.
+
+'''
